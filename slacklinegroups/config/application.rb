f2b67b3138b47d2fccf530032cf4bd4ece5f6034
Syntaf <gmercer015@gmail.com>
2020-04-27 21:59:10 -0700

    Configure cache store, sidekiq & redis

--- slacklinegroups/config/application.rb
+++ slacklinegroups/config/application.rb
@@ -18,6 +18,12 @@ module Slacklinegroups
     config.log_tags = %i[subdomain uuid]
     config.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT))
 
+    config.cache_store = :redis_cache_store, {
+      url: ENV['REDIS_URL'],
+      namespace: 'slacklinegroups::cache'
+    }
+
+    config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
