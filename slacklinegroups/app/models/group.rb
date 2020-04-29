39799cc14097db47f86b777b2e1c5d9f8bce26b5
Syntaf <gmercer015@gmail.com>
2020-04-28 20:30:04 -0700

    Refactor code to conform to rubocop

--- slacklinegroups/app/models/group.rb
+++ slacklinegroups/app/models/group.rb
@@ -1,18 +1,10 @@
+# frozen_string_literal: true
+
 require 'symbol_serializer'
 
 class Group < ApplicationRecord
-  GROUP_TYPES = %i[facebook_group facebook_page other]
+  GROUP_TYPES = %i[facebook_group facebook_page other].freeze
 
   serialize :type, Slg::SymbolSerializer
   validates :type, inclusion: { in: GROUP_TYPES }
-
-  # # Sets the group_type from a collection of allowable symbols. Enforces
-  # # the consistency of this field regardless of it's creation source.
-  # #
-  # # @param [Symbol] type One of :facebook_group, :facebook_page, :other
-  # def type=(type)
-  #   super(type.to_s) if types.presence_in(GROUP_TYPES)
-
-  #   raise InvalidGroupType, "Type of #{type.to_s} not supported"
-  # end
 end
--- slacklinegroups/lib/symbol_serializer.rb
+++ slacklinegroups/lib/symbol_serializer.rb
@@ -1,12 +1,18 @@
+# frozen_string_literal: true
+
 module Slg
+  # Serializer class for ActiveRecord model attributes. Useful
+  # for representing string columns as symbols within the domain
   class SymbolSerializer
     def self.dump(symbol)
       return unless symbol
+
       symbol.to_s
     end
 
     def self.load(db_string)
       return unless db_string
+
       db_string.to_sym
     end
   end
