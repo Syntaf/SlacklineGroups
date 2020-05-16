7c9da34386fd5d1bf1696d9f5c7e6c57cd597b08
Syntaf <gmercer015@gmail.com>
2020-05-16 12:08:07 -0700

    Write model associations for group, location, & info

--- slacklinegroups/app/models/group.rb
+++ slacklinegroups/app/models/group.rb
@@ -3,6 +3,9 @@
 require 'symbol_serializer'
 
 class Group < ApplicationRecord
+  has_one :info, dependent: :destroy
+  has_one :location, dependent: :destroy
+
   GROUP_TYPES = %i[facebook_group facebook_page other].freeze
 
   serialize :type, Slg::SymbolSerializer
