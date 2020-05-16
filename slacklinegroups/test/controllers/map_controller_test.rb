7cff0670300bf60f9b647090ffdc9c91f77f14a3
Syntaf <gmercer015@gmail.com>
2020-05-16 11:43:38 -0700

    Write controller test for map#index, direct root to map#index

--- slacklinegroups/test/controllers/map_controller_test.rb
+++ slacklinegroups/test/controllers/map_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class MapControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    get map_index_url
+    assert_response :success
+  end
 end
