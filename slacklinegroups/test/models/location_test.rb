42481cf5937ef58a61d380d657cbb454559f03df
Syntaf <gmercer015@gmail.com>
2020-05-16 12:06:05 -0700

    Create location model

--- slacklinegroups/test/models/location_test.rb
+++ slacklinegroups/test/models/location_test.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 require 'test_helper'
 
-class InfoTest < ActiveSupport::TestCase
+class LocationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
