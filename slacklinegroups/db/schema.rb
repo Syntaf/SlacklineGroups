e78a8ef7dc20bf408b94bcfa713e18ad66e30233
Syntaf <gmercer015@gmail.com>
2020-05-31 10:53:28 -0700

    Create devise Admin model

--- slacklinegroups/db/schema.rb
+++ slacklinegroups/db/schema.rb
@@ -10,15 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_190524) do
+ActiveRecord::Schema.define(version: 2020_05_31_175013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "slug"
-    t.string "gtype"
+    t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["slug"], name: "index_groups_on_slug", unique: true
