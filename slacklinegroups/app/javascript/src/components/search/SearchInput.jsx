9d7a30f6600bdb9ebd43cc552a2ec3fa11738062
Syntaf <gmercer015@gmail.com>
2020-06-14 13:50:07 -0700

    Show loading spinner while fetching groups

--- slacklinegroups/app/javascript/src/components/search/SearchInput.jsx
+++ slacklinegroups/app/javascript/src/components/search/SearchInput.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
 
 import TextField from '@material-ui/core/TextField';
-import { InputAdornment } from '@material-ui/core';
+import { InputAdornment, CircularProgress } from '@material-ui/core';
 
 import SearchIcon from '@material-ui/icons/Search';
 
 const SearchInput = props => {
+  const startAdornment = props.disabled ? (
+    <InputAdornment position='start' className='loadingContainer'>
+      <CircularProgress className='loading' size='20px' />
+    </InputAdornment>
+  ) : null;
+
+  console.log(startAdornment);
+
+  const endAdornment = (
+    <InputAdornment position='end'>
+      <SearchIcon />
+    </InputAdornment>
+  );
+
+  const inputProps = {
+    disableUnderline: true,
+    endAdornment: endAdornment,
+    startAdornment: startAdornment
+  };
+
   return (
     <TextField
       className='searchBar'
       label=''
       disabled={props.disabled}
       placeholder='Name or Location'
-      InputProps={{
-        disableUnderline: true,
-        endAdornment: (
-          <InputAdornment position="end">
-            <SearchIcon />
-          </InputAdornment>
-        )    
-      }}/>
+      InputProps={inputProps}/>
   )
 }
 
