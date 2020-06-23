de705b6d4a23b6d6cb41b430547195cc46a330c1
Syntaf <gmercer015@gmail.com>
2020-06-23 13:41:42 -0700

    Create GroupTile component and styling

--- slacklinegroups/app/javascript/src/components/map/GroupTile.jsx
+++ slacklinegroups/app/javascript/src/components/map/GroupTile.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 
-const GroupTile = props => {
+const GroupTile = ({ groupName, groupType, link}) => {
   return (
-    <Card raised={true}>
-      <h1>Group {props.hello}</h1>
-    </Card>
+    <React.Fragment>
+      <div className="header">
+        <h1>{groupName}</h1>
+      </div>
+      <div className="groupContent">
+        <ul>
+          <li>
+            <p className="label">Page Type:</p>
+            <span>{groupType}</span>
+          </li>
+        </ul>
+        <div className="imagePlaceholder">
+          <p>Images coming soon</p>
+        </div>
+        <div className="links">
+          <a className="groupLink" href={link}>Go to Group</a>
+          <a href="">Report an issue</a>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
