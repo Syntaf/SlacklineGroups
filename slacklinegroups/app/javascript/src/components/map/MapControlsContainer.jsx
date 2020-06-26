c8fbf71da6b3f52a4175824e714ac81d775c4566
Syntaf <gmercer015@gmail.com>
2020-06-26 12:27:55 -0700

    Create MapControlsContainer component

---
+++ slacklinegroups/app/javascript/src/components/map/MapControlsContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MapControlsContainer = props => (
+  <div class="mapControlsContainer">
+    {props.children}
+  </div>
+);
+
+export default MapControlsContainer;
