64601f7ff7bb6d6d3a867fe2b960cc0bf2e4d553
Syntaf <gmercer015@gmail.com>
2020-06-26 12:28:16 -0700

    Allow Map to accept children and render children after container

--- slacklinegroups/app/javascript/src/components/map/Map.jsx
+++ slacklinegroups/app/javascript/src/components/map/Map.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Map = React.forwardRef((props, ref) => (
+  <React.Fragment>
     <div id='map' ref={el => (ref.current = el)}></div>
+    {props.children}
+  </React.Fragment>
 ));
 
 export default Map;
