fba0e88a453bc09c929698f5662beba561119189
Syntaf <gmercer015@gmail.com>
2020-07-19 11:23:08 -0700

    Refactor spacing & simplify homeContainer

--- slacklinegroups/app/javascript/src/containers/homeContainer.jsx
+++ slacklinegroups/app/javascript/src/containers/homeContainer.jsx
@@ -6,12 +6,10 @@ import Home from '../scenes/home';
 
 const store = configureStore();
 
-const HomeContainer = ({ assets }) => {
-    return (
-        <Provider store={store}>
-            <Home assets={assets} />
-        </Provider>
-    );
-}
+const HomeContainer = ({ assets }) => (
+  <Provider store={store}>
+    <Home assets={assets} />
+  </Provider>
+);
 
 export default HomeContainer;
