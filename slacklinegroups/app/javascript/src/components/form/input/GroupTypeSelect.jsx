8f39e72b458f8b16f4dadf8a70ec18bceca8fa32
Syntaf <gmercer015@gmail.com>
2020-07-18 19:50:26 -0700

    Refactor group type select to be native

--- slacklinegroups/app/javascript/src/components/form/input/GroupTypeSelect.jsx
+++ slacklinegroups/app/javascript/src/components/form/input/GroupTypeSelect.jsx
@@ -3,17 +3,18 @@ import React from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
 
 const GroupTypeSelect = props => {
   return (
     <FormControl className="formControl">
       <InputLabel>Group Type</InputLabel>
-      <Select value={props.value} onChange={props.onChange}>
-        <MenuItem value="facebook_group">Facebook Group</MenuItem>
-        <MenuItem value="facebook_page">Facebook Page</MenuItem>
-        <MenuItem value="other">Other</MenuItem>
-      </Select>
+      <NativeSelect value={props.value} onChange={props.onChange}>
+        <option value=""></option>
+        <option value="facebook_group">Facebook Group</option>
+        <option value="facebook_page">Facebook Page</option>
+        <option value="other">Other</option>
+      </NativeSelect>
     </FormControl>
   )
 };
