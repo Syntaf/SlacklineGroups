fb4858ab634e443b6461fb89e7611b46da21b6d2
Syntaf <gmercer015@gmail.com>
2020-07-19 10:57:51 -0700

    Write tests for submitter model

--- slacklinegroups/test/models/submitter_test.rb
+++ slacklinegroups/test/models/submitter_test.rb
@@ -1,7 +1,27 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class SubmitterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'rejects invalid email' do
+    submitter = Submitter.new
+    submitter.group = groups(:one)
+
+    ['invalid@@gmail.com', 'invalid'].each do |email|
+      submitter.email = email
+
+      assert_not submitter.valid?
+    end
+  end
+
+  test 'accepts valid emails' do
+    submitter = Submitter.new
+    submitter.group = groups(:one)
+
+    ['admin@example.com', 'johndoe@gmail.com'].each do |email|
+      submitter.email = email
+
+      assert submitter.valid?
+    end
+  end
 end
