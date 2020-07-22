64f6bc30e144db5289caf21577551146447b8f31
Syntaf <gmercer015@gmail.com>
2020-07-21 19:18:52 -0700

    Create useMapManager hook

---
+++ slacklinegroups/app/javascript/src/hooks/MapManagerHook.js
@@ -0,0 +1,20 @@
+import { useState, useRef, useEffect } from 'react';
+import MapManagerFactory from '../lib/map/MapManagerFactory';
+
+/**
+ * React hook for rendering a map onto a page. Returns a reference that must
+ * be forwarded to a component via ref={mapRef}, as well as a manager for the map
+ * which can be used to visualize & add layers to a map.
+ * 
+ * @returns {Array}
+ */
+const useMapManager = () => {
+  const mapRef = useRef(null);
+  const [mapManager, setMapManager] = useState(null);
+
+  useEffect(() => { setMapManager(MapManagerFactory.create(mapRef)) }, []);
+
+  return [mapRef, mapManager];
+};
+
+export default useMapManager;
--- slacklinegroups/app/javascript/src/scenes/home.jsx
+++ slacklinegroups/app/javascript/src/scenes/home.jsx
@@ -1,30 +1,24 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { connect } from 'react-redux';
 import { fetchMapGroups } from '../actions/map';
-
-import { Zoom, Slide } from '@material-ui/core';
+import useMapManager from '../hooks/MapManagerHook';
 
 import Map from '../components/map/Map';
 import MapNavigationBar from '../components/navigation/MapNavigationBar';
-import MapManagerFactory from '../lib/map/MapManagerFactory';
 import MapControlsContainer from '../components/map/MapControlsContainer';
 import MapResetButton from '../components/navigation/MapResetButton';
 
 const Home = ({dispatch, isFetching, groups, assets}) => {
-  const mapContainer = useRef(null);
-  const [mapManager, setMapManager] = useState(null);
+  const [mapRef, mapManager] = useMapManager();
 
   /** Fetch groups on initial component load */
   useEffect(() => { dispatch(fetchMapGroups()); }, []);
-
-  /** Create and render the map without any visualizations initially */
-  useEffect(() => { if (!mapManager) setMapManager(MapManagerFactory.create(mapContainer)); }, [mapManager]);
 
   /** Add necessary sources, layers, and interaction handlers once groups have been received */
   useEffect(() => { if (mapManager && groups.length) mapManager.visualize(groups); }, [mapManager, groups]);
 
   return (
-    <Map ref={mapContainer} >
+    <Map ref={mapRef} >
       <MapControlsContainer>
         <MapNavigationBar disabled={isFetching} />
         <MapResetButton mapManager={mapManager} />
