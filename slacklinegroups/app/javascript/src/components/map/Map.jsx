66b51ec9dfee6a42d7d331ae8a7fe66f83d4e97f
Syntaf <gmercer015@gmail.com>
2020-07-30 10:47:20 -0700

    Place child props within map container

--- slacklinegroups/app/javascript/src/components/map/Map.jsx
+++ slacklinegroups/app/javascript/src/components/map/Map.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Map = React.forwardRef((props, ref) => (
   <React.Fragment>
-    <div id='map' ref={el => (ref.current = el)}></div>
-    {props.children}
+    <div id='map' ref={el => (ref.current = el)}>
+      {props.children}
+    </div>
   </React.Fragment>
 ));
 
