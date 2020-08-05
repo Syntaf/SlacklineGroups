114ae15230a152c93b65a3c711f0dd621358cb19
Syntaf <gmercer015@gmail.com>
2020-08-05 10:01:55 -0700

    Add helper docs for rails + solargraph

---
+++ slacklinegroups/rails.rb
@@ -0,0 +1,6 @@
+# @!parse
+#   class ActionController::Base
+#     include ActionController::MimeResponds
+#     extend ActiveSupport::Callbacks::ClassMethods
+#     extend AbstractController::Callbacks::ClassMethods
+#   end
