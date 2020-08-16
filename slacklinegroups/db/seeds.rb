51d1cd6384a6d6d937a9164510931e34c4d3a7f9
Syntaf <gmercer015@gmail.com>
2020-08-15 18:57:10 -0700

    Use type instead of gtype for seeds

--- slacklinegroups/db/seeds.rb
+++ slacklinegroups/db/seeds.rb
@@ -10,25 +10,25 @@ Moderator.create(email: 'mod@localhost', password: 'mod').confirm
 # -------------------------------------------------------------------------
 
 # ----------------------------- Specific Group Clusters -----------------------------
-vegasSlacklife = Group.new(name: 'Vegas Slacklife', slug: 'vegas-slacklife', gtype: :facebook_group)
+vegasSlacklife = Group.new(name: 'Vegas Slacklife', slug: 'vegas-slacklife', type: :facebook_group)
 vegasSlacklife.info = Info.new(link: 'https://www.facebook.com/groups/798358306957010', members: 15, is_regional: false)
 vegasSlacklife.location = Location.new(lat: 36.019375, lon: -115.1116391)
 vegasSlacklife.save
 
-lvSlacklineGroups = Group.new(name: 'LV Slackline Groups', slug: 'lv-slackline-group', gtype: :facebook_group)
+lvSlacklineGroups = Group.new(name: 'LV Slackline Groups', slug: 'lv-slackline-group', type: :facebook_group)
 lvSlacklineGroups.info = Info.new(link: 'https://www.facebook.com/groups/522046384501227', members: 5, is_regional: false)
 lvSlacklineGroups.location = Location.new(lat: 36.044763, lon: -115.268200)
 lvSlacklineGroups.save
 
-lvSlackers = Group.new(name: 'Las Vegas Slackers', slug: 'las-vegas-slackers', gtype: :facebook_page)
+lvSlackers = Group.new(name: 'Las Vegas Slackers', slug: 'las-vegas-slackers', type: :facebook_page)
 lvSlackers.info = Info.new(link: 'https://www.grantmercer.dev/', members: 30, is_regional: false)
 lvSlackers.location = Location.new(lat: 36.2185009, lon: -115.166914)
 
-wcSlackers = Group.create(name: 'West Coast Slackers', slug: 'west-coast-slackers', gtype: :facebook_group)
+wcSlackers = Group.create(name: 'West Coast Slackers', slug: 'west-coast-slackers', type: :facebook_group)
 wcSlackers.info = Info.new(link: 'https://www.grantmercer.dev/', members: 245, is_regional: true)
 wcSlackers.location = Location.new(lat: 40.573865, lon: -114.170715)
 
-sdSlackers = Group.create(name: 'San Diego Slackers', slug: 'san-diego-slackers', gtype: :facebook_group)
+sdSlackers = Group.create(name: 'San Diego Slackers', slug: 'san-diego-slackers', type: :facebook_group)
 sdSlackers.info = Info.new(link: 'https://www.grantmercer.dev/', members: 566, is_regional: false)
 sdSlackers.location = Location.new(lat: 32.733892, lon: -117.169497)
 
@@ -41,7 +41,7 @@ base_lon = 13.519466
 500.times do |n|
   cords = cord_generator.random_location(base_lon, base_lat, 1_300_000)
 
-  random_group = Group.new(name: "Random Group #{n}", slug: ('a'..'z').to_a.shuffle[0,8].join, gtype: :facebook_group)
+  random_group = Group.new(name: "Random Group #{n}", slug: ('a'..'z').to_a.shuffle[0,8].join, type: :facebook_group)
   random_group.info = Info.new(link: 'https://www.facebook.com', members: n, is_regional: false)
   random_group.location = Location.new(lon: cords.first, lat: cords.second)
   random_group.save
