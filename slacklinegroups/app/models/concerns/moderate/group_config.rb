36cad789cc02b752b93a97c3df96ca2a4cb7497d
Syntaf <gmercer015@gmail.com>
2020-08-15 19:23:21 -0700

    Define base configuration for group admin view

--- slacklinegroups/app/models/concerns/moderate/group_config.rb
+++ slacklinegroups/app/models/concerns/moderate/group_config.rb
@@ -4,20 +4,36 @@ module Moderate
   module GroupConfig
     extend ActiveSupport::Concern
 
+    VISIBLE_FIELD_ORDER = %i[approved name slug gtype approved info location submitter].freeze
+
+    class << self
+      # Defines the default configuration shared by all admin views relating to Group
+      #
+      # @return [Proc] A proc to be evaluated by a given admin configuration block
+      def base_configuration
+        proc do
+          configure :gtype, :active_record_enum do
+            label 'Type'
+          end
+
+          fields(*VISIBLE_FIELD_ORDER)
+        end
+      end
+    end
+
     included do
       rails_admin do
         edit do
-          field :slug, :string do
+          instance_eval(&Moderate::GroupConfig.base_configuration)
+
+          configure :slug, :string do
             read_only true
             help ''
           end
-          field :name, :string
-          field :type, :string
-          field :approved, :boolean
-          field :info
-          field :location
-          field :submitter
         end
+
+        list(&Moderate::GroupConfig.base_configuration)
+        show(&Moderate::GroupConfig.base_configuration)
       end
     end
   end
