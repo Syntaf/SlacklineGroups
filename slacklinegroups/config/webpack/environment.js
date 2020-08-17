39ae07f2f2454d892eecad852392c4675e443781
Syntaf <gmercer015@gmail.com>
2020-08-17 09:00:23 -0700

    Remove server rendering, split vendor & code JS

--- slacklinegroups/config/webpack/environment.js
+++ slacklinegroups/config/webpack/environment.js
@@ -1,5 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+environment.config.merge({
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+});
+
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
 
