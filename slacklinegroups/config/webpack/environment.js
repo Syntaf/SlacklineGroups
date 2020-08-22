82842647cd86410e2a69f673df4b4459b8714513
Syntaf <gmercer015@gmail.com>
2020-08-22 11:52:54 -0700

    Split vendor file into separate chunks

--- slacklinegroups/config/webpack/environment.js
+++ slacklinegroups/config/webpack/environment.js
@@ -1,9 +1,21 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
 environment.config.merge({
     optimization: {
         splitChunks: {
-            chunks: 'all'
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+                        return `npm.${packageName.replace('@', '')}`;
+                    }
+                }
+            }
         }
     }
 });
