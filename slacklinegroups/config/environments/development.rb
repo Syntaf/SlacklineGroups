070cb147ccf60a896acb05662425c5e63db0cab7
Syntaf <gmercer015@gmail.com>
2020-08-23 10:55:52 -0700

    Add asset configuration for mailers on dev/prod

--- slacklinegroups/config/environments/development.rb
+++ slacklinegroups/config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Configure email for development
   config.action_mailer.perform_deliveries = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.asset_host = 'localhost'
+  config.action_mailer.default_url_options = { host: 'localhost' }
   config.action_mailer.smtp_settings = {
     user_name: 'apikey',
     password: ENV['SENDGRID_PASSWORD'],
