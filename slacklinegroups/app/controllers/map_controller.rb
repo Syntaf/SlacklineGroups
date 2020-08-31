7ee6f4ad41300f6f6518ce457e078ca1b9a76804
Syntaf <gmercer015@gmail.com>
2020-08-31 09:03:30 -0700

    Define subdomain constraints for routes

--- slacklinegroups/app/controllers/map_controller.rb
+++ slacklinegroups/app/controllers/map_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class MapController < ApplicationController
+  before_action :set_view_context
+
   def index
-    @value = CounterJob.perform_now
+    logger.debug params
+  end
+
+  private
+
+  # Determines the view context of the given request. Available
+  # view contexts are :native and :embeded
+  def set_view_context
+    @view_context = request.subdomain == 'api' ? :embeded : :native
   end
 end
--- slacklinegroups/config/routes.rb
+++ slacklinegroups/config/routes.rb
@@ -3,18 +3,21 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin/manage', as: 'rails_admin'
-  get '/admin', to: redirect('/admin/manage')
-
-  authenticate :admin, ->(user) { user.is_a?(Overseer) } do
-    mount Sidekiq::Web => '/admin/monitor'
-  end
-
-  devise_for :admins, path: 'admin'
-
-  resources :groups, param: :slug
-  resources :map, only: %i[index]
-  resources :about, only: %i[index]
+  constraints subdomain: false do
+    mount RailsAdmin::Engine => '/admin/manage', as: 'rails_admin'
+    get '/admin', to: redirect('/admin/manage')
   
+    authenticate :admin, ->(user) { user.is_a?(Overseer) } do
+      mount Sidekiq::Web => '/admin/monitor'
+    end
+  
+    devise_for :admins, path: 'admin'
+  
+    resources :about, only: %i[index]
+  end
+  
+  resources :map, only: %i[index]
+  resources :groups, param: :slug
+
   root to: 'map#index'
 end
