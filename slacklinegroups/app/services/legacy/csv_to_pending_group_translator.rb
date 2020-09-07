1d74cbda374c0ef381ffb1f176fe61254c13338e
Syntaf <gmercer015@gmail.com>
2020-09-07 10:26:23 -0700

    Remove unneeded test_helper include

--- slacklinegroups/app/services/legacy/csv_to_pending_group_translator.rb
+++ slacklinegroups/app/services/legacy/csv_to_pending_group_translator.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'test_helper'
-
 module Legacy
   class CsvToPendingGroupTranslator < CsvToGroupTranslator
     EXPECTED_HEADERS = %w[id name group_type lat lon link email members is_regional created_at updated_at].freeze
